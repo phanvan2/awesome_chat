24afaeda5530f519496b2249aa0c2ef4509f082f
phanvan2 <phanvanphung215@gmail.com>
2021-07-08 15:34:38 +0700

    [1.0.0] connet DB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from 'express'; 
+import ConnectDB from "./config/connectDB" ; 
+import ContactModel from "./models/contact.model" ; 
 let app = express(); 
 
+ConnectDB() ; 
 let hostname = "localhost";
 let port = 5000; 
 
@@ -8,6 +11,20 @@ app.get("/hello" , (req , res) => {
     res.send("<h1> hello world </h1>") ; 
 }) ; 
 
+app.get("/test-database", async (req, res) => {
+    try{
+        let item = {
+            userId: "21242" , 
+            contactId: "4221",
+        } ; 
+        let contact = await ContactModel.createNew(item) ; 
+        res.send("ok rồi "); 
+    }catch(err){
+        console.log(err) ; 
+    }
+}); 
+
 app.listen( port , hostname, ()=> {
-    console.log("hello phan vann")
+    console.log(process.env) ; 
+    console.log("hello phan vann") ; 
 }) ; 
